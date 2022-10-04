8d1252fff5f1f9e7fe2eca3b6e6dbf72719a759f
Che S Jadallah <110256562+Sh0aib-Ja0allah@users.noreply.github.com>
2022-10-04 22:22:01 +0300

    Add files via upload

---
+++ Sho3lah/Data/Enums/Boards.cs
@@ -0,0 +1,14 @@
+﻿namespace Sho3lah.Data.Enums
+{
+
+    public enum Boards
+    {
+        president=0,
+        social_Boards =1,
+        Financial=2,
+        Curtural_Board=3,
+        Public_Relations=4,
+        vice_president=5,
+        Team_Member=6
+    }
+}
---
+++ Sho3lah/Data/Enums/BookCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace Sho3lah.Data.Enums
+{
+    public enum BookCategory
+    {
+        Fantasy=1,
+        Cookbook=2,
+        Art=3,
+        Mystery=4,
+        History=5,
+        Romance=6,
+        Motivational=7,
+        Criminal=8
+
+    }
+}
